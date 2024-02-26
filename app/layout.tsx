2c252f245abfbdd4055de244826b438b1c1f23c4
Truong Pham <truongpham2412.nd@gmail.com>
2024-02-26 22:10:36 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,11 @@ const RootLayout: React.FC<RootLayout> = ({ children }) => {
   return (
     <html lang="en">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
         rel="stylesheet"
